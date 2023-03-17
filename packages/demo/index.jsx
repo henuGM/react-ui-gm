69ae5658c7108bab1102bbb2d92fc162318cfc04
henuGM <1841113974@qq.com>
2023-03-17 15:57:57 +0800

    add:demo

--- packages/demo/index.jsx
+++ packages/demo/index.jsx
@@ -1,69 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SButton } from "react-ui-gm";
-import "react-ui-gm/dist/style.css";
-import FormDemo from "./form";
 import AsyncDemo from "./asyncComponent";
+import Keep from "./keep";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <h2>AsyncDemo</h2>
     <AsyncDemo></AsyncDemo>
-    <div style={{marginBottom:"20px"}}>
-      <SButton color="blue">主要按钮</SButton>
-      <SButton color="green">绿色按钮</SButton>
-      <SButton color="gray">灰色按钮</SButton>
-      <SButton color="yellow">黄色按钮</SButton>
-      <SButton color="red">红色按钮</SButton>
-    </div>
-    <div style={{marginBottom:"20px"}}>
-      <SButton color="blue" plain>
-        朴素按钮
-      </SButton>
-      <SButton color="green" plain>
-        绿色按钮
-      </SButton>
-      <SButton color="gray" plain>
-        灰色按钮
-      </SButton>
-      <SButton color="yellow" plain>
-        黄色按钮
-      </SButton>
-      <SButton color="red" plain>
-        红色按钮
-      </SButton>
-    </div>
-    <div style={{marginBottom:"20px"}}>
-      <SButton size="small" plain>
-        小按钮
-      </SButton>
-      <SButton size="medium" plain>
-        中按钮
-      </SButton>
-      <SButton size="large" plain>
-        大按钮
-      </SButton>
-    </div>
-    <div style={{marginBottom:"20px"}}>
-      <SButton color="blue" round plain icon="search">
-        搜索按钮
-      </SButton>
-      <SButton color="green" round plain icon="edit">
-        编辑按钮
-      </SButton>
-      <SButton color="gray" round plain icon="check">
-        成功按钮
-      </SButton>
-      <SButton color="yellow" round plain icon="message">
-        提示按钮
-      </SButton>
-      <SButton color="red" round plain icon="delete">
-        删除按钮
-      </SButton>
-    </div>
-    <div>
-      <FormDemo></FormDemo>
-
-    </div>
-
+    <br></br>
+    <h2>KeepAive</h2>
+    <Keep></Keep>
   </React.StrictMode>
 );
---
+++ packages/demo/keep.jsx
@@ -0,0 +1,111 @@
+import React ,{button,input}from "react";
+import {
+  KeepaliveItem,
+  KeepaliveScope,
+  useCacheDestroy
+} from "react-ui-gm";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate
+} from "react-router-dom";
+
+function CompForm() {
+  const [value, setValue] = React.useState("");
+  return (
+    <div>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+    </div>
+  );
+}
+
+function Atom({ propsNumber }) {
+  const [number, setNumber] = React.useState(0);
+  return (
+    <div>
+      props:{propsNumber} | 当前:{number}
+      <button onClick={() => setNumber(number + 1)}>add++</button>
+      <button onClick={() => setNumber(number - 1)}>del--</button>
+    </div>
+  );
+}
+
+function CompNumber() {
+  const [number, setNumber] = React.useState(0);
+  const [isShow, setShow] = React.useState(true);
+  return (
+    <div>
+      <p>this is a number component</p>
+      {isShow && <Atom propsNumber={number} />}
+      {isShow && (
+        <KeepaliveItem cacheId="number_atom">
+          <Atom propsNumber={number} />
+        </KeepaliveItem>
+      )}
+      <button onClick={() => setShow(!isShow)}>
+        局部 {isShow ? "隐藏" : "展示"}
+      </button>
+      <br />
+      <button onClick={() => setNumber(number + 1)}>add props</button>
+    </div>
+  );
+}
+
+function CompText() {
+  const destroy = useCacheDestroy();
+  return (
+    <div>
+      清除input的缓存
+      {/* 销毁 cacheId = form 的组件 */}
+      <button onClick={() => destroy("form")}>clean form cache</button>
+      <br></br>
+      清除number的缓存
+      {/* 销毁 cacheId = form 的组件 */}
+      <button onClick={() => destroy("number_atom")}>clean form cache</button>
+    </div>
+  );
+}
+/* 菜单栏组件 */
+function Menus() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      路由切换:
+      <button style={{ marginRight: "10px" }} onClick={() => navigate("/form")}>
+        输入页面
+      </button>
+      <button
+        style={{ marginRight: "10px" }}
+        onClick={() => navigate("/number")}
+      >
+        数字增减
+      </button>
+      <button style={{ marginRight: "10px" }} onClick={() => navigate("/text")}>
+        清除cache页面
+      </button>
+    </div>
+  );
+}
+
+export default function Keep() {
+  return (
+    <Router>
+      <Menus />
+      <KeepaliveScope>
+        <Routes>
+          <Route
+            element={
+              <KeepaliveItem cacheId="form">
+                <CompForm />
+              </KeepaliveItem>
+            }
+            path="/form"
+          />
+          <Route element={<CompNumber />} path="/number" />
+          <Route element={<CompText />} path="/text" />
+        </Routes>
+      </KeepaliveScope>
+    </Router>
+  );
+}
